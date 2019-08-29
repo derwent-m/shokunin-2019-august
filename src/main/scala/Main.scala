20cfaaa9f2517c1fea54c0c66f95dc8ba05574e3
Derwent <derwent.m@thoughtworks.com>
2019-08-29 16:18:20 +1000

    ✨Implement main

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,40 @@
+import solver._
+
+object Main extends App  {
+  val people = Vector(
+    "Je", "Ev", "Jo", "Sa", "Ma"
+  )
+  val n = people.length // number of people
+  val tree = ConstraintTree[Int](
+    Leaf(SingleConstraint(Vector(-1, 0, 0, 0, 0, -1))) // Je > 1 => -Je < -1
+  ).and(ConstraintTree[Int](
+    Leaf(SingleConstraint(Vector(0, 1, 0, 0, 0, n))) // Ev < n
+  )).and(ConstraintTree[Int](
+    Leaf(SingleConstraint(Vector(0, 0, -1, 0, 0, -1))) // Jo > 1 => -Jo < -1
+  )).and(ConstraintTree[Int](
+    Leaf(SingleConstraint(Vector(0, 0, 1, 0, 0, n))) // Jo < n
+  )).and(ConstraintTree[Int](
+    Leaf(SingleConstraint(Vector(0, -1, 0, 1, 0, 0))) // Sa > Ev => Ev - Sa < 0
+  )).and(
+    ConstraintTree[Int](
+      Leaf(SingleConstraint(Vector(0, 0, -1, 0, 1, -1))) // Ma < Jo - 1 => Ma - Jo < -1
+    ).or(ConstraintTree[Int](
+      Leaf(SingleConstraint(Vector(0, 0, 1, 0, -1, -1))) // Ma > Jo + 1 => Jo - Ma < -1
+    ))
+  ).and(
+    ConstraintTree[Int](
+      Leaf(SingleConstraint(Vector(0, -1, 1, 0, 0, -1))) // Jo < Ev - 1 => Jo - Ev < -1
+    ).or(ConstraintTree[Int](
+      Leaf(SingleConstraint(Vector(0, 1, -1, 0, 0, -1))) // Jo > Ev + 1 => Ev - Jo < -1
+    ))
+  )
+  // Ranks can be anywhere from 1 to n
+  val variables = (1 to n).map(_ => 1 to n).toVector
+  val solutions = Solver.bruteForceInequalities( tree, variables )
+  println(s"Solutions:")
+  for { solution <- solutions } {
+
+    val rankings = for ((ranking, person) <- solution.zipWithIndex) yield Tuple2(ranking, people(person))
+    println(rankings.sorted.map(tuple => tuple._2).mkString(", "))
+  }
+}
